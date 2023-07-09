0f0de35734f0d928d71f8c31ab0e9ca65ff95f86
pants007 <pense007@gmail.com>
2023-07-09 13:09:17 +0200

    test batching of images during video conversion, add option to output depthmaps, make use of changes described in previous commits
    Signed-off-by: pants007 <pense007@gmail.com>

--- vid3d.py
+++ vid3d.py
@@ -4,17 +4,25 @@ import argparse
 import os
 import subprocess as sp
 import numpy as np
-from gen_stereo import process_image
+from models import DepthModel
+from gen_stereo import process_image_element_wise, process_image_correct
+import PIL.Image as Image
+VIDEO_EXTS = ['.mp4', '.webm']
+IMAGE_EXTS = ['.png', '.jpg', '.jpeg']
+EXTENSIONS = tuple(VIDEO_EXTS + IMAGE_EXTS)
+process_image = process_image_element_wise
 
 
 def parseArguments():
-    parser = argparse.ArgumentParser(description='stereo.py')
+    parser = argparse.ArgumentParser(description='vid3d.py')
     parser.add_argument('--output_dir', type=str, nargs='?', default='./output/',
                         help='Optional output directory path')
     parser.add_argument('--divergence', type=float, nargs='?', default=4.0,
                         help='Determines how strong the 3D effect is')
     parser.add_argument('--sample_rate', type=float, nargs='?', default=30.0,
                         help='The FPS of the output video.')
+    parser.add_argument('--save-depth', type=bool, nargs="?", default=False,
+                        help='replaces the right-eye view with the generated depth map')
     args = parser.parse_args()
     return args
 
@@ -29,24 +37,16 @@ def get_video_metadata(video_path):
     return width, height, framerate
 
 
-def load_model():
-    repo = "isl-org/ZoeDepth"
-    model_zoe_n = torch.hub.load(repo, "ZoeD_N", pretrained=True)
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    zoe = model_zoe_n.to(DEVICE)
-    zoe.eval()
-    return zoe
-
-
-def get_file_list(directory):
-    file_list = []
-    extensions = ('.mp4', '.webm')
+def get_file_lists(directory):
+    img_list, vid_list = [], []
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(extensions):  # Modify extensions as needed
-                filename = file.split('.')[0]
+            print(file)
+            if file.endswith(EXTENSIONS):
+                filename, ext = os.path.splitext(file)
+                print(ext)
                 found = False
-                for extension in extensions:
+                for extension in EXTENSIONS:
                     stereo_file = os.path.join(
                         './output', f'{filename}_stereo{extension}')
                     if os.path.exists(stereo_file):
@@ -54,86 +54,119 @@ def get_file_list(directory):
                         found = True
                         break
                 if not found:
-                    file_list.append(file)
+                    if ext in IMAGE_EXTS:
+                        img_list.append(file)
+                    if ext in VIDEO_EXTS:
+                        vid_list.append(file)
 
-    return file_list
+    return img_list, vid_list
 
 
-args = parseArguments()
-model = load_model()
-files = get_file_list('./input')
+if __name__ == '__main__':
+    args = parseArguments()
+    model = DepthModel('ZoeDepth', 'MiDaS_small')
+    imgs, vids = get_file_lists('./input')
+    print(f'images: {imgs}\nvideos: {vids}')
 
-for file in files:
-    video_path = f'./input/{file}'
+    print('Processing images now..')
+    for file in imgs:
+        print(f'Processing {file}..')
+        img_path = f'./input/{file}'
+        filename, ext = os.path.splitext(file)
+        image = Image.open(img_path)
+        # ignore this for now. it's just extra processing that isn't related to my width-doubling woes.
+        # Do some image processing here...
+        depth_numpy = model.infer(image)
+        lr_frame = process_image(
+            image, depth_numpy, args.divergence)
+        output_filename = f'{filename}_stereo.png'
+        output_file = f'./output/{output_filename}'
+        Image.fromarray(lr_frame, "RGB").save(output_file)
 
-    width, height, framerate = get_video_metadata(video_path)
-    filename = os.path.splitext(file)[0]
-    output_filename = filename + '_stereo.mp4'
-    output_file = f'./output/{output_filename}'
-    print(f'Beginning to process {output_file}..')
-    command_in = [
-        'ffmpeg',
-        '-hide_banner',
-        '-loglevel', 'error',
-        '-i', video_path,
-        '-vf', 'fps=30',
-        '-f', 'image2pipe',
-        '-pix_fmt', 'rgb24',
-        '-vcodec', 'rawvideo', '-'
-    ]
-    pipe_in = sp.Popen(command_in, stdout=sp.PIPE)
+    print('Processing videos now..')
+    for file in vids:
+        print(f'Processing {file}..')
+        video_path = f'./input/{file}'
 
-    # Start a subprocess that runs ffmpeg and takes its input from a pipe
-    command_out = [
-        'ffmpeg',
-        '-hide_banner',
-        '-loglevel', 'error',
-        '-stats',
-        '-y',  # overwrite output file if it exists
-        '-f', 'rawvideo',
-        '-s', f'{width*2}x{height}',  # size of one frame
-        '-pix_fmt', 'rgb24',
-        '-r', '30',  # output frame rate
-        '-i', '-',  # take input from stdin
-        '-an',  # no audio
-        '-vcodec', 'libx264',
-        output_file
-    ]
-    pipe_out = sp.Popen(command_out, stdin=sp.PIPE)
+        width, height, framerate = get_video_metadata(video_path)
+        filename, ext = os.path.splitext(file)
+        output_filename = f'{filename}_stereo{ext}'
+        output_file = f'./output/{output_filename}'
+        print(f'Beginning to process {output_file}..')
+        command_in = [
+            'ffmpeg',
+            '-hide_banner',
+            '-loglevel', 'error',
+            '-i', video_path,
+            '-vf', 'fps=30',
+            '-f', 'image2pipe',
+            '-pix_fmt', 'rgb24',
+            '-vcodec', 'rawvideo', '-'
+        ]
+        pipe_in = sp.Popen(command_in, stdout=sp.PIPE)
 
-    # Process video frame by frame
-    try:
-        while True:
-            # Read one frame's worth of data from the input pipe
-            raw_image = pipe_in.stdout.read(width * height * 3)
-            if not raw_image:
-                break
+        # Start a subprocess that runs ffmpeg and takes its input from a pipe
+        command_out = [
+            'ffmpeg',
+            '-hide_banner',
+            '-loglevel', 'error',
+            '-stats',
+            '-y',  # overwrite output file if it exists
+            '-f', 'rawvideo',
+            '-s', f'{width*2}x{height}',  # size of one frame
+            '-pix_fmt', 'rgb24',
+            '-r', '30',  # output frame rate
+            '-i', '-',  # take input from stdin
+            '-an',  # no audio
+            '-vcodec', 'libx264',
+            output_file
+        ]
+        pipe_out = sp.Popen(command_out, stdin=sp.PIPE)
 
-            # Convert the raw image data to a numpy array
-            image = np.frombuffer(raw_image, dtype='uint8').reshape(
-                [height, width, 3]).copy()
-            # ignore this for now. it's just extra processing that isn't related to my width-doubling woes.
-            # Do some image processing here...
-            depth_numpy = model.infer_pil(image)
-            lr_frame = process_image(
-                image, depth_numpy, args.divergence)
-            # Write the processed frame to the output pipe
-            pipe_out.stdin.write(np.array(lr_frame).astype(np.uint8).tobytes())
-            # pipe_out.stdin.write(processed_image.astype(np.uint8).tobytes())
-    except KeyboardInterrupt:
-        pipe_out.stdin.flush()
+        # Process video frame by frame
+        try:
+            while True:
+                # Read one frame's worth of data from the input pipe
+                raw_image = pipe_in.stdout.read(width * height * 3 * 5)
+                if not raw_image:
+                    break
+
+                # Convert the raw image data to a numpy array
+                images = np.frombuffer(raw_image, dtype='uint8').reshape(
+                    [5, height, width, 3]).copy()
+                lr_frames = []
+                # ignore this for now. it's just extra processing that isn't related to my width-doubling woes.
+                # Do some image processing here...
+                for i in range(images.shape[0]):
+                    image = images[i]
+                    depth_numpy = model.infer(image)
+                    if not args.save_depth:
+                        lr_frame = process_image(
+                            image, depth_numpy, args.divergence)
+                        lr_frames.append(lr_frame)
+                    else:
+                        img_np = np.array(image)
+                        depth_numpy = (depth_numpy * 255).astype(np.uint8)
+                        depth = np.stack((depth_numpy,)*3, axis=-1)
+                        lr_frames.append(np.hstack((img_np, depth)))
+                lr_frames = np.array(lr_frames)
+                # Write the processed frame to the output pipe
+                pipe_out.stdin.write(lr_frames.astype(np.uint8).tobytes())
+                # pipe_out.stdin.write(processed_image.astype(np.uint8).tobytes())
+        except KeyboardInterrupt:
+            pipe_out.stdin.flush()
+            pipe_out.terminate()
+            pipe_out.stdin.close()
+            pipe_out.wait()
+            pipe_in.stdout.flush()
+            pipe_in.terminate()
+            pipe_in.stdout.close()
+            pipe_in.wait()
+
         pipe_out.terminate()
         pipe_out.stdin.close()
         pipe_out.wait()
-        pipe_in.flush()
         pipe_in.terminate()
         pipe_in.stdout.close()
         pipe_in.wait()
-
-    pipe_out.terminate()
-    pipe_out.stdin.close()
-    pipe_out.wait()
-    pipe_in.terminate()
-    pipe_in.stdout.close()
-    pipe_in.wait()
-    print('Done!')
+        print('Done!')
